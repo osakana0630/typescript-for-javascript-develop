70e9cc3a91527d6310310a6f764704bc00a88b23
osakana0630 <osakana0630@gmail.com>
2020-12-12 04:35:41 +0900

    ts lesson

---
+++ src/array.ts
@@ -0,0 +1,16 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+
+let numbers2: Array<number> = [1,2,3];
+let strings2: Array<string> = ["Tokyo", "Osaka", "Kyoto"];
+
+let strings: string[] = ["react", "vue", "next"];
+
+let ary: number[][] = [
+    [50, 100],
+    [150, 300]
+];
+
+let ary2: (string | number | boolean)[] = [1, false, "Japan"];
+
---
+++ src/tuple.ts
@@ -0,0 +1,5 @@
+export {};
+
+let profile: [string, number] = ["naoya", 24];
+// profile = [24, "naoya"];
+
